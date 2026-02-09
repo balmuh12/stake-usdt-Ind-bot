ddcdf0426790a3e17e76556fa0e6180f0cf343b5
balmuh12 <privatepribadi94@gmail.com>
2026-02-09 19:26:50 +0700

    Add configuration for Telegram admin and investment packages

---
+++ config.py
@@ -0,0 +1,9 @@
+# Daftar admin Telegram ID
+ADMINS = [123456789]  # Ganti dengan Telegram ID admin
+
+# Paket investasi
+PACKAGES = {
+    "Basic": {"min_deposit": 100000, "duration": 30, "profit_per_day": 10000},
+    "Premium": {"min_deposit": 300000, "duration": 30, "profit_per_day": 35000},
+    "VIP": {"min_deposit": 1000000, "duration": 30, "profit_per_day": 120000}
+}
